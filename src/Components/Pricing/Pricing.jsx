2b86574b93a17484191988d8fd2a077b66b8ad51
officialshahidulshihab <officialshahidulshihab@gmail.com>
2026-03-31 15:07:54 +0600

    pricing card  section resposive  done

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -7,11 +7,11 @@ const Pricing = ({pricingPromise}) => {
     
     return (
         <div className='mt-10 w-11/12 mx-auto'>
-            <p className='text-[38px] font-bold text-[#101727] text-center'>Simple, Transparent Pricing</p>
-            <p className='text-[#627382] text-center'>Choose the plan that fits your needs. Upgrade or downgrade anytime.</p>
+            <p className=' text-[20px] md:text-[38px] font-bold text-[#101727] text-center'>Simple, Transparent Pricing</p>
+            <p className='text-[#627382] text-center text-[14px] md:text-[16px]'>Choose the plan that fits your needs. Upgrade or downgrade anytime.</p>
 
 
-            <div className='grid grid-cols-3 gap-5'>
+            <div className='grid grid-cols-1  md:grid-cols-3 gap-5'>
                 {
                 pricingData.map((price,i)=><PricingDisplay price={price} key={i}></PricingDisplay>)
             }
--- src/Components/Pricing/PricingDisplay.jsx
+++ src/Components/Pricing/PricingDisplay.jsx
@@ -9,15 +9,15 @@ const PricingDisplay = ({ price }) => {
   return (
     <div className="mt-7 mb-7 relative">
       {
-        price.highlighted? <p className="absolute -top-3 left-40   badge badge-warning">{price.tag}</p>: ''
+        price.highlighted? <p className="absolute -top-3 left-30  md:left-40   badge badge-warning">{price.tag}</p>: ''
       }
       
         <div className={` ${price.highlighted? 'card-body p-5 rounded-lg bg-linear-to-r from-[#4f39f6] to-[#9514fa]' : 'card-body bg-base-200 border-base-300 p-5 rounded-lg '}`}>
           
 
-        <p className={`text-[24px] font-bold ${price.highlighted? "text-white" :"text-[#101727]"} `}>{price.name}</p>
+        <p className={` text-[20px] md:text-[24px] font-bold ${price.highlighted? "text-white" :"text-[#101727]"} `}>{price.name}</p>
         <p className={` ${price.highlighted? "text-white" :"text-[#627382]"}`}>{price.tagline}</p>
-        <p className=""><span className={`text-[30px] font-bold ${price.highlighted? "text-white" :"text-[#101727]"}`}>${price.monthlyPrice}</span><span className={` ${price.highlighted? "text-white" :"text-[#627382]"}`}>/{price.period}</span></p>
+        <p className=""><span className={`text-[20px] md:text-[30px] font-bold ${price.highlighted? "text-white" :"text-[#101727]"}`}>${price.monthlyPrice}</span><span className={` ${price.highlighted? "text-white" :"text-[#627382]"}`}>/{price.period}</span></p>
        {
         features.map((item, i)=><PricingFeatures price={price} item={item} key={i} ></PricingFeatures>)
        }
