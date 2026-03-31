6699d30b11061678069d3c0a341ddfd3a23439a8
officialshahidulshihab <officialshahidulshihab@gmail.com>
2026-03-31 13:44:50 +0600

    pricing section completed

--- src/Components/Pricing/PricingDisplay.jsx
+++ src/Components/Pricing/PricingDisplay.jsx
@@ -7,14 +7,25 @@ const PricingDisplay = ({ price }) => {
   
   
   return (
-    <div className="">
-        <div className="bg-base-100 border-base-300 p-5 rounded-lg ">
+    <div className="mt-7 mb-7 relative">
+      {
+        price.highlighted? <p className="absolute -top-3 left-40   badge badge-warning">{price.tag}</p>: ''
+      }
+      
+        <div className={` ${price.highlighted? 'card-body p-5 rounded-lg bg-linear-to-r from-[#4f39f6] to-[#9514fa]' : 'card-body bg-base-200 border-base-300 p-5 rounded-lg '}`}>
+          
 
-        <p>{price.name}</p>
-        <p>{price.tagline}</p>
-        <p>$ {price.monthlyPrice}/{price.period}</p>
+        <p className={`text-[24px] font-bold ${price.highlighted? "text-white" :"text-[#101727]"} `}>{price.name}</p>
+        <p className={` ${price.highlighted? "text-white" :"text-[#627382]"}`}>{price.tagline}</p>
+        <p className=""><span className={`text-[30px] font-bold ${price.highlighted? "text-white" :"text-[#101727]"}`}>${price.monthlyPrice}</span><span className={` ${price.highlighted? "text-white" :"text-[#627382]"}`}>/{price.period}</span></p>
        {
-        features.map((item, i)=><PricingFeatures item={item} key={i} ></PricingFeatures>)
+        features.map((item, i)=><PricingFeatures price={price} item={item} key={i} ></PricingFeatures>)
+       }
+
+       {/* <button className="mt-3 bg-base-100 rounded-2xl"><span className={`${price.highlighted? 'btn   rounded-2xl w-full bg-base-100  bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-transparent bg-clip-text ' :'btn bg-linear-to-r from-[#4f39f6] to-[#9514fa]  rounded-2xl text-white w-full'}  `}>{price.cta}</span></button> */}
+
+       {
+        price.highlighted ?  <button className=" btn bg-white rounded-2xl w-full mt-3"><span className="bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-transparent bg-clip-text">{price.cta}</span></button> : <button className="btn bg-linear-to-r from-[#4f39f6] to-[#9514fa]  rounded-2xl text-white w-full mt-3">{price.cta}</button>
        }
 
 
@@ -25,3 +36,5 @@ const PricingDisplay = ({ price }) => {
 };
 
 export default PricingDisplay;
+
+
--- src/Components/Pricing/PricingFeatures.jsx
+++ src/Components/Pricing/PricingFeatures.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { FaCheck } from 'react-icons/fa';
 
-const PricingFeatures = ({item}) => {
+const PricingFeatures = ({item ,price}) => {
     return (
         <div>
             <ul>
-                <li>{item.text}</li>
+                <li className={`flex gap-1 items-center ${price.highlighted? 'text-white' :'text-[#627382]'}  `}><FaCheck className='text-green-500' />{item.text}</li>
             </ul>
         </div>
     );
